5b2364f985bc6b707a7594e9826467d6be659b44
Alexandre Bessard <alexandre.bessard@gmail.com>
2023-11-06 16:32:16 -0500

    about and top skills sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {AboutComponent} from "./main-page/about/about.component";
+import {TopSkillsComponent} from "./main-page/top-skills/top-skills.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainPageComponent,
+    AboutComponent,
+    TopSkillsComponent
   ],
   imports: [
     BrowserModule,
