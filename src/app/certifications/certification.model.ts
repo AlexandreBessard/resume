15d386277163e79ce878b77a8b2a70d0325bf858
Alexandre Bessard <alexandre.bessard@gmail.com>
2023-11-16 21:52:18 -0500

    certifications imgs

--- src/app/certifications/certification.model.ts
+++ src/app/certifications/certification.model.ts
@@ -2,9 +2,18 @@
 
   constructor(
     public title: string,
+    public providedBy: string,
     public isExpired = false,
     public image: string,
-    public category: string) {
+    public category: Category) {
   }
 
+ }
+
+ export enum Category {
+   AWS = "AWS",
+   JAVA = "JAVA",
+   OTHER = "OTHER",
+   DEVOPS = "DEVOPS",
+   SPRING = "SPRING"
  }
--- src/app/certifications/certification.service.ts
+++ src/app/certifications/certification.service.ts
@@ -1,6 +1,6 @@
-import { CertificationModel } from './certification.model';
-import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import {Category, CertificationModel} from './certification.model';
+import {Injectable} from '@angular/core';
+import {from, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,18 +9,59 @@ export class CertificationService {
 
   private certifications: CertificationModel[] = [
     { title: "Verified International Academic Qualifications",
+      providedBy: "World Education Services",
+      isExpired: false,
+      image: "assets/certifications/wes.png",
+      category: Category.OTHER
+    },
+    { title: "AWS Certified Solutions Architect - Associate",
+      providedBy: "Amazon Web Services",
       isExpired: false,
       image: "assets/certifications/aws_architect.png",
-      category: "aws"
+      category: Category.AWS
     },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "aws" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: true, image: "image_url", category: "aws" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "java" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "java" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "java" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "other" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "other" },
-    { title: "AWS Certified Solutions Architect - Associate", isExpired: false, image: "image_url", category: "other" }
+    { title: "AWS Certified Developer - Associate",
+      providedBy: "Amazon Web Services",
+      isExpired: true,
+      image: "assets/certifications/aws_developer.png",
+      category: Category.AWS
+    },
+    { title: "Oracle Certified Associate, Java SE 8 Programmer",
+      providedBy: "Oracle",
+      isExpired: false,
+      image: "assets/certifications/java_8_associate.png",
+      category: Category.JAVA
+    },
+    { title: "Hashicorp Certified: Terraform Associate (002)",
+      providedBy: "HashiCorp",
+      isExpired: false,
+      image: "assets/certifications/terraform.png",
+      category: Category.DEVOPS
+    },
+    { title: "CKAD: Certified Kubernetes Application Developer",
+      providedBy: "The Linux Foundation",
+      isExpired: false,
+      image: "assets/certifications/kubernetes.png",
+      category: Category.DEVOPS
+    },
+    { title: "Oracle Certified Professional, Java SE 8 Programmer",
+      providedBy: "Oracle",
+      isExpired: false,
+      image: "assets/certifications/java_8_pro.png",
+      category: Category.JAVA
+    },
+    { title: "Oracle Certified Professional: Java SE 11 Developer",
+      providedBy: "Oracle",
+      isExpired: false,
+      image: "assets/certifications/java_11_pro.png",
+      category: Category.JAVA
+    },
+    { title: "VMware Spring Professional 2021",
+      providedBy: "Oracle",
+      isExpired: false,
+      image: "assets/certifications/spring_framework.png",
+      category: Category.SPRING
+    }
   ];
 
   getCertifications(): Observable<CertificationModel[]> {
