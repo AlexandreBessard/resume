0ce55977a2b6414ede0ac7bc31694019b8556eab
Alexandre Bessard <alexandre.bessard@gmail.com>
2024-01-02 20:38:31 +0100

    added new project

--- src/app/projects/projects.content.ts
+++ src/app/projects/projects.content.ts
@@ -4,7 +4,10 @@ export class ProjectsContent {
 
   static readonly projectsContent: ProjectModel[] = [
 
-    new ProjectModel("1", "CRUD application", "https://github.com/AlexandreBessard/crud-application", "imgUrl", "https://alexandrebessard.github.io/crud-application",
+    new ProjectModel("1", "CRUD application with Angular",
+      "https://github.com/AlexandreBessard/crud-application",
+      "imgUrl",
+      "https://alexandrebessard.github.io/crud-application",
       new ProjectDetail(
         "<h4>Employee Management:</h4>\n" +
         "    <ul>\n" +
@@ -43,10 +46,37 @@ export class ProjectsContent {
       ),
 
 
-    new ProjectModel("2", "Project in progress...", "", "", "",
-      new ProjectDetail("In progress, come back later...",
+    new ProjectModel("2", "Full Stack Web App with Java, Spring and Angular",
+      "https://github.com/AlexandreBessard/fullstack-app-using-java-angular",
+      "",
+      "https://crud-demo-f0c69e71d3bb.herokuapp.com/",
+      new ProjectDetail("<h1>Your Web Application</h1>\n" +
+        "    <p>Full Stack web application built with Angular and Spring Boot! Once an existing user is successfully logged in, " +
+        "you can do CRUD operations. The state is persisted in PostgreSQL database.</p>\n" +
+        "\n" +
+        "    <h2>Login Page</h2>\n" +
+        "    <p>Access the app by logging in with the following credentials using <em>Incognito mode</em>:</p>\n" +
+        "    <ul>\n" +
+        "        <li><strong>Username:</strong> admin</li>\n" +
+        "        <li><strong>Password:</strong> admin</li>\n" +
+        "    </ul>\n" +
+        "\n" +
+        "    <h2>Registration</h2>\n" +
+        "    <p>If you want to use another account, you can register a new one by clicking on the 'Register' button from the Logging page.</p>\n" +
+        "\n" +
+        "    <h2>Dashboard</h2>\n" +
+        "    <p>Once logged in, you'll have access to the dashboard where you can manage users by saving, deleting, or updating user information.</p>\n" +
+        "\n" +
+        "    <h2>JWT Token</h2>\n" +
+        "    <p>The login page provides a valid JWT token to protect routes and applies API authentication.</p>\n" +
+        "\n" +
+        "    <h2>Deployment</h2>\n" +
+        "    <p>The application is packaged using Maven as one executable JAR file, and it is deployed on Heroku. This allows you to build and run the app in the cloud. The app uses a PostgreSQL database to save the state, ensuring data persistence even after signing out.</p>\n" +
+        "<h3>Application architecture:</h3>" +
+        "<p>The application leverages a robust architectural structure, employing PostgreSQL as the database, which is hosted on Heroku. The client-side is developed using Angular, while the server-side components, including the Controller, Service, and Model, are powered by Spring Boot:</p>\n" +
+        "<img src=\"assets/projects/id2/architecture.png\" alt=\"Your Image Description\">\n",
         ["Java", "Angular"],
-      ""))
+      "This web application is designed with a clean and user-friendly interface. The main components include a Login Page, Registration functionality, a Dashboard for user management utilizing NGRX for state management, and features to secure routes using JWT tokens and API authentication hosted on Heroku cloud provider."))
   ];
 
 }
